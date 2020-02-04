40ad654bf4c626e2fa2af5b8cb0d6f3bb940cbf1
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-02-04 23:32:16 +0100

    Register User : Hash password and save user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -25,8 +26,28 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('User registred')
-  }
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+      user = new User({
+        name,
+        email,
+        password
+      });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+
+      res.send('User registred');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  } 
 );
 
 module.exports = router;
