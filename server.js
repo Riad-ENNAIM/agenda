a161e3d206231f286c5152a34c8f52a60327b7fb
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-02-04 23:14:19 +0100

    Connect MongoDB + User Model + Register Validation

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const auth = require('./routes/auth');
 const users = require('./routes/users');
 const contacts = require('./routes/contacts');
 
 const app = express();
+
+// Connect Database
+connectDB()
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 // Routes
 app.use('/api/auth', auth)
