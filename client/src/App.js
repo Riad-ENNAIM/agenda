d925fda731d37f1d7b69f095ddc240fa064d5e08
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-03-23 20:39:14 +0100

    Navbar & Router Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 import './App.css';
 
 const App = () => {
-  return <div className="App">Riad ENNAIM</div>;
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </>
+    </Router>
+  );
 }
 
 export default App;
