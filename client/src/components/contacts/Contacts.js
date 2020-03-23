7c29537a6b16198ce4560a91cb3012227abb9290
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-03-23 23:18:16 +0100

    Contacts Components & Setup Context

---
+++ client/src/components/contacts/Contacts.js
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import ContactContext from '../../context/contact/contactContext';
+import ContactItem from './ContactItem';
+
+const Contacts = () => {
+  const contactContext = useContext(ContactContext);
+
+  const { contacts } = contactContext;
+
+  return (
+    <>
+      {
+        contacts.map(contact => (
+          <ContactItem key={contact.id} contact={contact} />
+        ))
+      }
+    </>
+  );
+}
+
+export default Contacts;
---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,58 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+  SET_ALERT,
+  REMOVE_ALERT
+} from '../types';
+
+const ContactState = props => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'contact 1',
+        email: 'contact1@gmail.com',
+        phone: '111-222-333-444',
+        type: 'personal'
+      },
+      {
+        id: 2,
+        name: 'contact 2',
+        email: 'contact2@gmail.com',
+        phone: '111-222-333-444',
+        type: 'professional'
+      },
+      {
+        id: 3,
+        name: 'contact 3',
+        email: 'contact3@gmail.com',
+        phone: '111-222-333-444',
+        type: 'personal'
+      },
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
