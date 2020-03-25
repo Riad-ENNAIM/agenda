e95aea716aa1597961fe92a5eca7399cf83873ed
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-03-25 21:39:39 +0100

    Add Contact Form

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,4 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -43,11 +42,17 @@ const ContactState = props => {
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
+  // Add Contact
+  const addContact = contact => {
+    contact.id = Math.random();
+    dispatch({ type: ADD_CONTACT, payload: contact })
+  }
 
   return (
     <ContactContext.Provider
       value={{
-        contacts: state.contacts
+        contacts: state.contacts,
+        addContact
       }}
     >
       {props.children}
---
+++ client/src/context/contact/contactReducer.js
@@ -0,0 +1,26 @@
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+  SET_ALERT,
+  REMOVE_ALERT
+} from '../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload]
+      };
+      break;
+  
+    default:
+      return state;
+      break;
+  }
+}
