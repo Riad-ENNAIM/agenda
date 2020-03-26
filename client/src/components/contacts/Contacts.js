e1f0e608b999c5f38b8be59e1e2302737b39ad00
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-03-26 16:16:52 +0100

    Basic Animation with react-transition-group

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem';
 
@@ -13,17 +14,27 @@ const Contacts = () => {
 
   return (
     <>
-      {
-        filtered !== null
-        ?
-          filtered.map(contact => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))
-        :
-          contacts.map(contact => (
-            <ContactItem key={contact.id} contact={contact} />
-          ))
-      }
+      <TransitionGroup>
+          {filtered !== null
+            ? filtered.map(contact => (
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames='item'
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))
+            : contacts.map(contact => (
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames='item'
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))}
+        </TransitionGroup>
     </>
   );
 }
