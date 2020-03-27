8e5e6b94f5925133ec2824d47a01325ec074851b
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-03-27 14:51:52 +0100

    Register & Login Forms

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
@@ -19,6 +21,8 @@ const App = () => {
               <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </>
