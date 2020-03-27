05b022a90c00d82b275bbd7563b7ec2802352270
Riad-ENNAIM <riad.ennaim.info@gmail.com>
2020-03-27 13:54:56 +0100

    Auth Context

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,27 @@ import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
+import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
